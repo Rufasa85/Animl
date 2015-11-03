3cb9e0bf95ab763b8ce88f02e8af2ffe6118ce5f
Joe Rehfuss <joe.rehfuss@gmail.com>
2015-11-03 13:06:35 -0800

    maps now accept sightings added by user, update with different colored pin

--- controllers/search.js
+++ controllers/search.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../models')
 //allows me to accept zipcode, return county FIPS code for use in the BISON API
 var zip2Fips = require('../zip2fips.json');
 var request= require('request');
@@ -10,17 +11,33 @@ router.route('/').get(function(req, res){
 }).post(function(req,res){
 	var county = req.body.county || zip2Fips[req.body.zip];
 	console.log(county);
+	var reportedSightings = [];
+	db.animal.find({
+		where:{
+			name: req.body.animal
+		}
+	}).then(function(animal){
+		db.sighting.findAll({
+			where:{
+				animalId: animal.id
+			}
+		}).then(function(sightings){
+			sightings.forEach(function(sighting){
+				reportedSightings.push({lat:sighting.latitude, lng:sighting.longitude})
+			})
+		})
+	})
 	request(
 		('http://bison.usgs.ornl.gov/api/search.json?species='+ req.body.animal +'&type=common_name&countyFips='+county+'&count=100'),
 		function(error, response, body) {
 			if(!error && response.statusCode === 200) {
 				var data = JSON.parse(body);
-				var sightings = [];
 				var animals = data.data;
+				var sightings = [];
 				animals.forEach(function (animal){
 						sightings.push({lat:animal.decimalLatitude, lng:animal.decimalLongitude});
 				})
-				res.render('search/show', {sightings:sightings, animal:req.body.animal} )
+				res.render('search/show', {sightings:sightings, animal:req.body.animal, reportedSightings:reportedSightings} )
 			}	
 		}
 	);
--- static/js/map.js
+++ static/js/map.js
@@ -1,15 +1,24 @@
 var map;
+console.log(reportedSightings)
 
 function initMap() {
 	map = new google.maps.Map(document.getElementById("mapz"), {
 		center: {lat:parseFloat(sightings[0].lat), lng:parseFloat(sightings[0].lng)},
 		zoom:9
-	})
+	});
 	sightings.forEach(function(sighting){
 		var position = {lat: parseFloat(sighting.lat), lng:parseFloat(sighting.lng)};	
 		var marker = new google.maps.Marker({
     		position:position,
     		map: map
+		});
+	});
+	reportedSightings.forEach(function(report){
+		var position = {lat: parseFloat(report.lat), lng:parseFloat(report.lng)};	
+		var marker = new google.maps.Marker({
+    		position:position,
+    		map: map,
+    		icon: 'http://library.csun.edu/images/google_maps/marker-blue.png'
+		});
 	})
-  });
 }
