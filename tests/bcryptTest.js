fa94fa3fd3af348fceaaf1ba2dfc4d45d22c1835
Joe Rehfuss <joe.rehfuss@gmail.com>
2015-11-04 10:04:57 -0800

    moved some files around and removed test directory from git

--- tests/bcryptTest.js
+++
@@ -1,8 +0,0 @@
-var bcrypt = require('bcrypt');
-var password = 'password';
-bcrypt.hash(password, 10, function (err, hash){
-	console.log(password)
-	password = hash;
-	//console.log(hash);
-	//console.log(password)
-})
